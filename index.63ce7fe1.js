d1d85df98df46ebbd19c8959ba835e49352e8d33
Animus93 <98660420+Animus93@users.noreply.github.com>
2022-10-11 17:44:34 +0000

    Deploying to gh-pages from @ Animus93/goit-js-hw-10-food-service@2719288eb3cbea8b922c9970b58852fd728ef7ac 🚀

---
+++ index.63ce7fe1.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#theme-switch-toggle"),t=document.querySelector("body"),s="light-theme",a="dark-theme";""===localStorage.getItem("themeValue")?t.classList.add(s):t.classList.add(localStorage.getItem("themeValue")),e.addEventListener("change",(function(){t.classList.contains(s)?(t.classList.add(a),t.classList.remove(s)):t.classList.contains(a)&&(t.classList.add(s),t.classList.remove(a));localStorage.setItem("themeValue",t.classList.value)})),console.log(localStorage.getItem("themeValue"))}();
+//# sourceMappingURL=index.63ce7fe1.js.map
